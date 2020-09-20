bba1cb2479a4bb25be042d620ca5b84fa6251063
Dame <dcampbell7095@gmail.com>
2020-09-20 14:23:26 -0400

    removing all arrays and replacing with a single string of all values also adding in confirms for uppercase lowercase whole numbers and special characters and finally, adding in for loop to generate and return random password

--- script.js
+++ script.js
@@ -1,82 +1,84 @@
 // Arrays for characters
 
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-var specialCharacters = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "?",
-  "?",
-  ":",
-  "/",
-  "|",
-];
-
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+// var values = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+//   "G",
+//   "H",
+//   "I",
+//   "J",
+//   "K",
+//   "L",
+//   "M",
+//   "N",
+//   "O",
+//   "P",
+//   "Q",
+//   "R",
+//   "S",
+//   "T",
+//   "U",
+//   "V",
+//   "W",
+//   "X",
+//   "Y",
+//   "Z",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+//   "!",
+//   "@",
+//   "#",
+//   "$",
+//   "%",
+//   "^",
+//   "&",
+//   "*",
+//   "(",
+//   ")",
+//   "?",
+//   "?",
+//   ":",
+//   "/",
+//   "|",
+//   "0",
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+// ];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -89,22 +91,31 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function passwordLength() {
-  var howLong = prompt("How long would you like for your password to be?");
-
-  var confirmNumbers = confirm("Do you confirm to the use of whole numbers?");
-
-  var confirmCharacters = confirm(
-    "Do you confirm the use of special characters?"
-  );
-}
-
 function generatePassword() {
-  return "This is where my generated password will go";
+
+  var howLong = Number(prompt("Enter a number for how long you would like your password to be?"));
+
+  var values = "ABCDEFGHIJKLMNOPQRSTUCWXYZ0123456789!@#$%^&*()+|?/";
+
+  var confirmUpperCase = confirm("Click OK to confirm the use of upper case letters.")
+
+  var confirmLowerCase = confirm("Click OK to confirm the use of lowercase letters.")
+
+  var confirmNumbers = confirm("Click OK to confirm the use of whole numbers.");
+
+  var confirmCharacters = confirm("Click OK to confirm the use of special characters.");
+
+  var retVal = "";
+
+  for (var i = 0; i < howLong; i++) {
+    retVal += values.charAt(Math.floor(Math.random() * values.length))
+    
+  }
+  return retVal;
 }
 
 // Add event listener to generate button
 
-generateBtn.addEventListener("click", passwordLength);
+generateBtn.addEventListener("click", generatePassword);
 
 generateBtn.addEventListener("click", writePassword);
