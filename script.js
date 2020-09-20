73b1ea0f645b724738aba405ce2a5d7564a1b7c8
Dame <dcampbell7095@gmail.com>
2020-09-20 09:03:17 -0400

    Adding Password length function that will be executed when clicking the generate password button

--- script.js
+++ script.js
@@ -7,8 +7,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function passwordLength() {
+  var numbers = prompt("How long would you like for your password to be?");
+
+  var confirmNumber = confirm("Do you confirm to the use of whole numbers?")
+
+  var confirmCharacters = confirm("Do you confirm the use of special characters?")
+  
+}
+
+function generatePassword() {
+  return "This is where my generated password will go";
 }
 
 // Add event listener to generate button
+
+generateBtn.addEventListener("click", passwordLength);
+
 generateBtn.addEventListener("click", writePassword);
